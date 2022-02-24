d06595d38c870b379f7d57a4eb028e939c9774b8
Siddhesh Khandagale <90148705+Siddheshk02@users.noreply.github.com>
2022-02-24 19:08:57 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+const (
+	DB_USER     = "postgres"
+	DB_PASSWORD = "Sid@2002"
+	DB_NAME     = "Movies"
+)
+
+type Movie struct {
+	MovieID   string `json:"movieid"`
+	MovieName string `json:"moviename"`
+}
+
+type JsonResponse struct {
+	Type    string  `json:"type"`
+	Data    []Movie `json:"data"`
+	Message string  `json:"message"`
+}
+
+func setupDB() *sql.DB {
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
+	db, err := sql.Open("postgres", dbinfo)
+
+	checkErr(err)
+
+	return db
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func printMessage(message string) {
+	fmt.Println("")
+	fmt.Println(message)
+	fmt.Println("")
+}
+
+func GetMovies(w http.ResponseWriter, r *http.Request) {
+	db := setupDB()
+
+	printMessage("Getting Movies.....")
+
+	rows, err := db.Query("SELECT * FROM Movies")
+
+	checkErr(err)
+
+	var Movies []Movie
+
+	for rows.Next() {
+		var id int
+		var movieID string
+		var movieName string
+
+		err = rows.Scan(&id, &movieID, &movieName)
+
+		checkErr(err)
+
+		Movies = append(Movies, Movie{MovieID: movieID, MovieName: movieName})
+	}
+	json.NewEncoder(w).Encode(Movies)
+}
+
+func CreateMovie(w http.ResponseWriter, r *http.Request) {
+	movieID := r.FormValue("movieid")
+	movieName := r.FormValue("moviename")
+
+	var response = JsonResponse{}
+
+	if movieID == "" || movieName == "" {
+		response = JsonResponse{Type: "error", Message: "You are missing movieID or movieName parameter"}
+	} else {
+		db := setupDB()
+
+		printMessage("Inserting movie into DB")
+
+		fmt.Println("Inserting new movie with ID: " + movieID + " and name: " + movieName)
+
+		var lastInsertID int
+
+		_, err := db.Query("INSERT INTO movies(movieID, movieName) VALUES('$1','$2') returning id;", movieID, movieName).Scan(&lastInsertID)
+
+		checkErr(err)
+
+		response = JsonResponse{Type: "success", Message: "The movie has been inserted successfully!"}
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
+func DeleteMovie(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	movieID := params["movieid"]
+
+	var response = JsonResponse{}
+
+	if movieID == "" {
+		response = JsonResponse{Type: "error", Message: "You are missing movieID parameter."}
+	} else {
+		db := setupDB()
+
+		printMessage("Deleting movie from DB")
+
+		_, err := db.Exec("DELETE FROM movies where movieID = $1", movieID)
+
+		checkErr(err)
+
+		response = JsonResponse{Type: "success", Message: "The movie has been deleted successfully!"}
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func DeleteMovies(w http.ResponseWriter, r *http.Request) {
+	db := setupDB()
+
+	printMessage("Deleting all movies...")
+
+	_, err := db.Exec("DELETE FROM movies")
+
+	checkErr(err)
+
+	printMessage("All movies have been deleted successfully!")
+
+	var response = JsonResponse{Type: "success", Message: "All movies have been deleted successfully!"}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/movies", GetMovies).Methods("GET")
+
+	router.HandleFunc("/movies", CreateMovie).Methods("POST")
+
+	router.HandleFunc("/movies/{movieid}", DeleteMovie).Methods("DELETE")
+
+	router.HandleFunc("/movies", DeleteMovies).Methods("DELETE")
+
+	fmt.Println("Server at 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
